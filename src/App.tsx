828b663213021ec84f847118dd52cbaa18d61ed2
Ariane Mateus <ariane.ccomp@gmail.com>
2020-08-12 20:30:16 -0300

    feat: gesture

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 import Scroll from './pages/Scroll';
 import Login from './pages/Login';
+import Drag from './pages/Drag';
 
 const App: React.FC = () => {
-  return <Scroll />;
+  return <Drag />;
 };
 
 export default App;

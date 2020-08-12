d1411d657ac6bb8bc299c576ae3dd1e2e6c9278d
Ariane Mateus <ariane.ccomp@gmail.com>
2020-08-12 18:12:53 -0300

    feat: animated images

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,57 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {View, StyleSheet, StatusBar} from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   Easing,
+  interpolate,
+  Extrapolate,
 } from 'react-native-reanimated';
+
+import Img from './assets/hero.png';
+
 const App: React.FC = () => {
-  const titlePosition = useSharedValue(30);
+  const titlePosition = useSharedValue(100);
+  const imagePosition = useSharedValue(-100);
 
   useEffect(() => {
-    titlePosition.value = withTiming(0, {
-      duration: 2000,
-      easing: Easing.bounce,
-    });
-  }, [titlePosition.value]);
+    imagePosition.value = withTiming(
+      0,
+      {
+        duration: 1000,
+        easing: Easing.bounce,
+      },
+      () => {
+        titlePosition.value = withTiming(0, {
+          duration: 1000,
+          easing: Easing.bounce,
+        });
+      },
+    );
+  }, [titlePosition.value, imagePosition.value]);
 
   const titleStyle = useAnimatedStyle(() => {
     return {
       transform: [{translateY: titlePosition.value}],
+      opacity: interpolate(
+        titlePosition.value,
+        [100, 0],
+        [0, 1],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: imagePosition.value}],
     };
   });
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#13131a" />
+      <Animated.Image source={Img} style={[styles.hero, imageStyle]} />
       <Animated.Text style={[styles.text, titleStyle]}>Fala Ari</Animated.Text>
     </View>
   );
@@ -41,6 +69,12 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 32,
     fontWeight: 'bold',
+  },
+
+  hero: {
+    width: 288,
+    height: 200,
+    marginBottom: 40,
   },
 });
 export default App;
